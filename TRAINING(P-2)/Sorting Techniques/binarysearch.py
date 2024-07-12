69428e70e0fb516bf5608bcdfd4e3352007af521
SUBHASHVILLA <140956125+subhashvilla@users.noreply.github.com>
2024-07-12 21:30:13 +0530

    Update binarysearch.py

--- TRAINING(P-2)/Sorting Techniques/binarysearch.py
+++ TRAINING(P-2)/Sorting Techniques/binarysearch.py
@@ -1,22 +1,22 @@
-# Binary Search 
+Binary Search 
  
-# nums = [12, 34, 21, 23, 44, 56];
-# target = 23
-# nums = sorted(nums)
-# left, right = 0, len(nums) - 1 
-# found = False
+nums = [12, 34, 21, 23, 44, 56];
+target = 23
+nums = sorted(nums)
+left, right = 0, len(nums) - 1 
+found = False
  
-# while left <= right:
-#     mid = (left + right) // 2 
-#     if nums[mid] == target:
-#         found = True 
-#         break 
-#     elif nums[mid] < target:
-#         left = mid + 1 
-#     else:
-#         right = mid - 1 
+while left <= right:
+    mid = (left + right) // 2 
+    if nums[mid] == target:
+        found = True 
+        break 
+    elif nums[mid] < target:
+        left = mid + 1 
+    else:
+        right = mid - 1 
  
-# if found == True:
-#     print("Target")
-# else:
-#     print("Target nf")
+if found == True:
+    print("Target")
+else:
+    print("Target nf")
